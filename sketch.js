fb7ac43c84d798677713651911db0c9d1dc33862
ved-10 <76608430+ved-10@users.noreply.github.com>
2021-06-19 19:03:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+ 
+  
+  bg= loadImage("aladdin_cave.jpg")
+  //load image for the treasure background
+  bg2 = loadImage("treasure.jpg")
+}
+
+function setup() {
+  createCanvas(1500,700);
+  security = new Security();
+  system = new System();
+  
+  
+  
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+// add code for changing the background to the treasure background
+  
+
+  if(score === 3) {
+    clear()
+    background(bg2)
+    fill("black")
+    textSize(100);
+    text("TREASURE UNLOCKED",50, 400);
+  }
+
+  drawSprites()
+}
